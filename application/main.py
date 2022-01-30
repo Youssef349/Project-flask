2c30eeb2b38bf2c3271d1e8b6d623236bcc0e686
Youssef349 <82970800+Youssef349@users.noreply.github.com>
2022-01-30 19:23:43 +0100

    Update and rename routes.py to main.py

--- application/main.py
+++ application/main.py
@@ -1,5 +1,4 @@
 import imp
-from application import app
 from flask import appcontext_popped, render_template, url_for
 import pandas as pd
 import json
@@ -74,11 +73,40 @@ def main():
 
  ############## Dataframe
  worlds=pd.read_csv('teams.csv')
+ worlds['Rank'] = ['1st', '2nd', '3rd','1st', '2nd', '3rd','1st', '2nd', '3rd','1st', '2nd', '3rd','1st', '2nd', '3rd','1st', '2nd', '3rd','1st', '2nd', '3rd','1st', '2nd', '3rd','1st', '2nd', '3rd','1st', '2nd', '3rd','1st', '2nd', '3rd']
+ worlds = worlds.fillna(method='ffill')
  print(worlds)
+
+ data = {'Year':['2021','2021' ,'2021' ,'2020','2020','2020','2019','2019','2019','2018','2018','2018','2017','2017','2017','2016','2016','2016','2015','2015','2015','2014','2014','2014','2013','2013','2013','2012','2012','2012','2011','2011','2011' ], 
+        'Team':['EDward Gaming','DAMWON Gaming','Gen.G Esports','DAMWON Gaming','Suning','Top Esports','FunPlus Phoenix','G2 Esports','Invictus Gaming','Invictus Gaming','Fnatic','G2 Esports','Samsung Galaxy','SK Telecom T1','Royal Never Give Up','SK Telecom T1','Samsung Galaxy','ROX Tigers','SK Telecom T1','KOO Tigers','Origen','Samsung White','Star Horn Royal Club','Samsung Blue','SK Telecom T1 K','Royal Club','NaJin Black Sword','Taipei Assassins','Azubu Frost','Moscow Five','Fnatic','against All authority','Team SoloMid'], 
+        'CashPrize_en_USD':[489500,333750,178000,556250,389375,200250,834375,300375,155750,2418750,870750,451500,1723722,620540,321761,2028000,760500,380250,1000000,250000,150000,1000000, 250000,150000,1000000,250000,150000,1000000,250000,150000, 50000,25000,10000], 
+        } 
+  
+ df = pd.DataFrame(data) 
+
+ data1 = {'Team':['Azubu Frost','DAMWON Gaming','EDward Gaming','Fnatic','FunPlus Phoenix','G2 Esports','Gen.G Esports','Invictus Gaming','KOO Tigers','Moscow Five','NaJin Black Sword','Origen','ROX Tigers','Royal Club','Royal Never Give Up','SK Telecom T1','SK Telecom T1 K','Samsung Blue','Samsung Galaxy','Samsung White','Star Horn Royal Club','Suning','Taipei Assassins','Team SoloMid','Top Esports','against All authority'],  
+        'Times_in_the_top_3':[1,2,1,2,1,2,1,2,1,1,1,1,1,1,1,3,1,1,2,1,1,1,1,1,1,1],
+        } 
+  
+ lf = pd.DataFrame(data1) 
+
+ data2 = {'Team':['Azubu Frost','DAMWON Gaming','EDward Gaming','Fnatic','FunPlus Phoenix','G2 Esports','Gen.G Esports','Invictus Gaming','KOO Tigers','Moscow Five','NaJin Black Sword','Origen','ROX Tigers','Royal Club','Royal Never Give Up','SK Telecom T1','SK Telecom T1 K','Samsung Blue','Samsung Galaxy','Samsung White','Star Horn Royal Club','Suning','Taipei Assassins','Team SoloMid','Top Esports','against All authority'],  
+        'Total_Cash_Won':[250000,890000,489500,920750,834375,751875,178000,2574500,250000,150000,150000,150000,380250,250000,321761,3648540,1000000,150000,2484222,1000000,250000,389375,1000000,10000,200250,25000],
+        } 
+ tf = pd.DataFrame(data2)
+
+ graph_best_team = plt.subplots(figsize=(50,10))
+ graph_best_team=plt.bar(lf['Team'],lf['Times_in_the_top_3'],1.0,color='b')
+ plt.savefig('graph1.png')
+
+ plt.clf()
+ graph_More_Cash_Won = plt.subplots(figsize=(50,10))
+ graph_More_Cash_Won=plt.bar(tf['Team'],tf['Total_Cash_Won'],1.0,color='r')
+ plt.savefig('graph2.png')
 
  return None
 
-
+  
 
 if __name__== "__main__":
-   app.run()
+   main()
